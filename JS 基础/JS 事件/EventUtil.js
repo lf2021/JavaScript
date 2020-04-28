4406995c69eb63d6b3df0de2f9a096a23b405adc
Lee_Van <lf_tdcq@163.com>
2020-04-29 01:13:06 +0800

    温故

--- JS 基础/JS 事件/EventUtil.js
+++ JS 基础/JS 事件/EventUtil.js
@@ -1,47 +1,56 @@
-var EventUtil={
-    //检测绑定事件
-  addHandler:function (element,type,handler) {
-      if (element.addEventListener){
-          element.addEventListener(type,handler,false);
-      }else if (element.attachEvent){
-          element.attachEvent('on'+type,handler);
-      }else{
-          element['on'+type]=handler;
-      }
+var EventUtil = {
+  /**绑定事件
+   * element是元素结点
+   * type事件类型
+   * handler是事件句柄
+   */
+  addHandler: function (element, type, handler) {
+    if (element.addEventListener) {
+      // 除了IE8 以下
+      element.addEventListener(type, handler, false);
+    } else if (element.attachEvent) {
+      // IE8 以下
+      element.attachEvent("on" + type, handler);
+    } else {
+      element["on" + type] = handler;
+    }
   },
 
-
-  //通过removeHandler
-    removeHandler:function (element,type,handler) {
-        if (element.removeEventListener){
-            element.removeEventListener(type,handler,false);
-        }else if (element.detachEvent){
-            element.detachEvent('on'+type,handler)
-        }else{
-            element['on'+type]=null;
-        }
-    },
-
-    getEvent:function (event) {
-        return event?event:window.event;
-    },
-    getTarget:function (event) {
-        return event.target||event.srcElement;
-    },
-    preventDefault:function (event) {
-        if (event.preventDefault){
-            event.preventDefault();
-        }else{
-            event.returnValue=false;
-        }
-    },
-    stopPropagation: function (event) {
-        if (event.stopPropagation){
-            event.stopPropagation();
-        }else {
-            event.cancelBubble=true;
-        }
+  /**去除事件
+   * element是元素节点
+   * type事件类型
+   * handler是事件句柄
+   */
+  removeHandler: function (element, type, handler) {
+    if (element.removeEventListener) {
+      element.removeEventListener(type, handler, false);
+    } else if (element.detachEvent) {
+      element.detachEvent("on" + type, handler);
+    } else {
+      element["on" + type] = null;
     }
+  },
 
-
+  getEvent: function (event) {
+    return event ? event : window.event;
+  },
+  getTarget: function (event) {
+    return event.target || event.srcElement;
+  },
+  preventDefault: function (event) {
+    if (event.preventDefault) {
+      event.preventDefault();
+    } else {
+      event.returnValue = false;
+    }
+  },
+  stopPropagation: function (event) {
+    if (event.stopPropagation) {
+      // W3C 阻止冒泡
+      event.stopPropagation();
+    } else {
+      // IE 阻止冒泡
+      event.cancelBubble = true;
+    }
+  },
 };
